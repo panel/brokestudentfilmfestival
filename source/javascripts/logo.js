ab18de88089d21b0058570a8117c49c50499cb7d
Paul Nelson <panel@Pauls-MacBook-Pro.local>
2013-12-15 20:43:54 -0600

    Initial commit with landing and portfolio pages.

---
+++ source/javascripts/logo.js
@@ -0,0 +1,51 @@
+(function(window, $, undefined) {
+	var BSFF = window.BSFF || {};
+
+	BSFF.logo = function(container, panelSelector) {
+		var that = {};
+
+		that.$container = $(container);
+		that.$panels = $(panelSelector);
+
+		function _getRandomDuration(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+		function _cycleImages($panel) {
+			var images = $panel.children('img');
+			var active = images.filter('.active');
+			var next = images.filter('[data-order="' + active.data('next') + '"]');
+
+			next.fadeIn().addClass('active');
+
+			if(active.data('order') === 0) {
+				var afterNext = (images.length - 1 === next.data('order')) ? 1 : parseInt(next.data('order'), 10) + 1;
+				active.data('next', afterNext);
+			} else {
+				active.fadeOut();
+			}
+
+			active.removeClass('active');
+		}
+
+		that.bindPanels = function() {
+			that.$panels.each(function(index, el) {
+				setInterval(function() {
+					_cycleImages($(el));
+				}, _getRandomDuration(1000, 3000));
+			});
+		};
+
+		that.cycleImages = _cycleImages;
+
+		return that;
+	};
+
+	window.BSFF = BSFF;
+
+}(window, jQuery));
+
+$(function() {
+	var logo = window.BSFF.logo('#logo', '.logo-panel');
+	logo.bindPanels();
+});
